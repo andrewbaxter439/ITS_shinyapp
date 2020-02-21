9bb2b47968da3613a30097a087f7aaa8cc6c6c06
Andrew Baxter <andrewbaxter439@gmail.com>
2020-02-21 16:38:46 +0000

    revert to DT::renderDataTable due to shiny rendering failure in cloud

--- server.R
+++ server.R
@@ -208,8 +208,8 @@ server <- function(input, output, session) {
   # Dataframe setup --------------------------------------------------------------------------------------------
   
   load("data/all_uk_rates.rdata")
-  # output$fulldata <- shiny::renderDataTable(modcfac())
-  output$fulldata <- shiny::renderDataTable(all_UK_rates[[input$ages]], options = list(searching = FALSE, paging = FALSE, info = FALSE, ordering = FALSE))
+  # output$fulldata <- DT::renderDataTable(modcfac())
+  output$fulldata <- DT::renderDataTable(all_UK_rates[[input$ages]], options = list(searching = FALSE, paging = FALSE, info = FALSE, ordering = FALSE))
   
   dfa <- reactive({
     all_UK_rates[[input$ages]] %>% 
@@ -277,7 +277,7 @@ server <- function(input, output, session) {
   #     geom_point() + geom_smooth(method = "lm", se = FALSE)
   # })  # Simple plot (not used)
   
-  output$dataframesumm <- shiny::renderDataTable(
+  output$dataframesumm <- DT::renderDataTable(
     (arrange(dfc(), by=Year)),
     options = list(searching = FALSE, paging = FALSE, info = FALSE, ordering = FALSE)
   )
@@ -617,7 +617,7 @@ server <- function(input, output, session) {
   
   # Outputting tables ------------------------------------------------------------------------------------------
   
-  output$confint<- shiny::renderDataTable(confintervals() %>% select(-Std.Error), 
+  output$confint<- DT::renderDataTable(confintervals() %>% select(-Std.Error), 
                                    options = list(searching = FALSE, paging = FALSE, info = FALSE, ordering = FALSE))
   
   confintervals <- reactive({
@@ -629,7 +629,7 @@ server <- function(input, output, session) {
   
   
   
-  output$modelsummary <- shiny::renderDataTable({
+  output$modelsummary <- DT::renderDataTable({
     req(input$int1yr)
     modelTable()}, options = list(searching = FALSE, paging = FALSE, info = FALSE, ordering = FALSE)
   )
@@ -710,7 +710,7 @@ server <- function(input, output, session) {
   })
   
   
-  output$cfac <- shiny::renderDataTable(modcfac())
+  output$cfac <- DT::renderDataTable(modcfac())
   
   ylim <- reactive({
     c(0, 1.1*max(modcfac()$HiCI, dfd()$Value))
@@ -731,7 +731,7 @@ server <- function(input, output, session) {
       mutate(Predict = predict(model, newdata = .))# Add Predicts for non-England
   })
   
-  output$dfd <- shiny::renderDataTable(dfd(), options = list(ordering = FALSE))
+  output$dfd <- DT::renderDataTable(dfd(), options = list(ordering = FALSE))
   
   
   # Output equation --------------------------------------------------------------------------------------------
@@ -918,7 +918,7 @@ server <- function(input, output, session) {
   # autocorr tests ---------------------------------------------------------------------------------------------
   
   
-  output$dwt <- shiny::renderDataTable(
+  output$dwt <- DT::renderDataTable(
     (
       data.frame(lag = 1:12, dwt(modelGls(), max.lag = 12, alternative = "two.sided")[1:3]) %>% rename(Autocorrelation =r, DW_Stat = dw, pvalue=p)
     ),
@@ -960,7 +960,7 @@ server <- function(input, output, session) {
   
   output$pplus1_title <- renderText({paste0("ANOVA comparison with model ", pplus1_text(), ":")})
   
-  output$pplus1 <- shiny::renderDataTable(options = list(searching = FALSE, paging = FALSE, info = FALSE), {
+  output$pplus1 <- DT::renderDataTable(options = list(searching = FALSE, paging = FALSE, info = FALSE), {
     
     model <- modelGls_null()
     model$call[[2]] <- mod_formula()
@@ -975,7 +975,7 @@ server <- function(input, output, session) {
   
   output$qplus1_title <- renderText({paste0("ANOVA comparison with model ", qplus1_text(), ":")})
   
-  output$qplus1 <- shiny::renderDataTable(options = list(searching = FALSE, paging = FALSE, info = FALSE), {
+  output$qplus1 <- DT::renderDataTable(options = list(searching = FALSE, paging = FALSE, info = FALSE), {
     model <- modelGls_null()
     model$call[[2]] <- mod_formula()
     
--- ui.R
+++ ui.R
@@ -124,11 +124,11 @@ ui <- function(request){
                           div(id = "equation",
                            column(12, align = "center", uiOutput(outputId = "equation"))
                           ),
-                           shiny::dataTableOutput(outputId = "modelsummary")
+                           DT::dataTableOutput(outputId = "modelsummary")
                            ),
                   
                   tabPanel("Confidence Intervals",
-                                      shiny::dataTableOutput((outputId = "confint")),
+                                      DT::dataTableOutput((outputId = "confint")),
                            # textOutput("form2"),
                            br(),
                            downloadButton("dlconfints")
@@ -138,17 +138,17 @@ ui <- function(request){
                            h3("Autocorrelation plots"), p("Residuals plotted by time, and autocorrelation and partial-autocorrelation function plots"),
                            plotOutput("autocorr"),
                            h3("Durbin-Watson test"),
-                           shiny::dataTableOutput("dwt"),
+                           DT::dataTableOutput("dwt"),
                            br(),
                            # htmlOutput(outputId = "corrcompare")
                            h4(textOutput("pplus1_title")),
-                           shiny::dataTableOutput("pplus1"),
+                           DT::dataTableOutput("pplus1"),
                            h4(textOutput("qplus1_title")),
-                           shiny::dataTableOutput("qplus1")
+                           DT::dataTableOutput("qplus1")
                            ),
                   
-                  tabPanel("Dataframe for model", shiny::dataTableOutput(outputId = "dataframesumm"))
-                  # tabPanel("Dataframe for model", shiny::dataTableOutput(outputId = "fulldata"))
+                  tabPanel("Dataframe for model", DT::dataTableOutput(outputId = "dataframesumm"))
+                  # tabPanel("Dataframe for model", DT::dataTableOutput(outputId = "fulldata"))
       )
     )
   )
